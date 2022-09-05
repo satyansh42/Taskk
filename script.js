3938d679426f041a65a15297135023803c8ef82d
Satyansh Prajapati <190106042@hbtu.ac.in>
2022-09-05 23:47:23 +0530

    initial

---
+++ script.js
@@ -0,0 +1,140 @@
+document.getElementById('Private').onclick = function PrivateClick() {
+    document.getElementById('Private_radio').checked = true
+    document.getElementById('Commertial_radio').checked = false
+    scrollNext();
+    progress();
+}
+document.getElementById('Commertial').onclick = function CommertialClick() {
+    document.getElementById('Commertial_radio').checked = true
+    document.getElementById('Private_radio').checked = false
+    scrollNext();
+    progress();
+}
+
+document.getElementById('Paper').onclick = function PaperClick() {
+    document.getElementById('Paper_radio').checked = true
+    document.getElementById('Smart_radio').checked = false
+    scrollNext();
+    progress();
+}
+document.getElementById('Smart').onclick = function SmartClick() {
+    document.getElementById('Smart_radio').checked = true
+    document.getElementById('Paper_radio').checked = false
+    scrollNext();
+    progress();
+}
+
+// function validateForm(){
+let nameee = document.forms["form1"]["name"];
+let nameBool;
+let place = document.forms["form2"]["place"];
+let placeBool = false;
+let years = document.forms["form2"]["years"];
+let yearsBool = false;
+let years2 = document.forms["form2"]["years2"];
+let years2Bool = false;
+const NatureOfDl = () => {
+    if(document.getElementById('Private_radio').checked || document.getElementById('Commertial_radio').checked){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+let TypeOfDl = () => {
+    if(document.getElementById('Paper_radio').checked || document.getElementById('Smart_radio').checked){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+nameee.onchange = function validName(){
+    if (nameee.value == "") {
+        nameBool = false;
+    }
+    else{
+        nameBool = true;
+        scrollNext();
+    }
+}
+place.onchange = function validName(){
+    if (place.value == "") {
+        placeBool = false;
+    }
+    else{
+        placeBool = true;
+        scrollNext();
+    }
+}
+years.onchange = function validName(){
+    if (years.value == "") {
+        yearsBool = false;
+    }
+    else{
+        yearsBool = true;
+        scrollNext();
+    }
+}
+
+function scrollNext(){
+    console.log(NatureOfDl())
+    console.log(nameBool)
+    console.log(TypeOfDl())
+    if(nameBool && NatureOfDl() && TypeOfDl()){
+        let scrollcont = document.getElementById('form_container');
+        scrollcont.scrollLeft = scrollcont.scrollWidth
+        console.log('scrolled');
+    }
+}
+
+let ddd = true;
+document.getElementById('select_s').onclick = function dd(){
+        ddd = !ddd;
+        function dddd(){
+            if (ddd===true){
+                return 'flex'
+            }
+            else{
+                return 'none'
+            }
+        }
+        document.getElementById('select_options').style.display = dddd();
+        document.getElementById('form_container').scrollTop = document.getElementById('form_container').scrollHeight;
+}
+document.getElementById('options').onclick = function ds(){
+    document.getElementById('select_options').innerHTML = document.getElementById('options').innerHTML;
+}
+function progress(){
+    let x = 0;
+    if(NatureOfDl()){
+        x = x+1;
+    }
+    if(nameBool){
+        x = x+1;
+    }
+    if(TypeOfDl()){
+        x = x+1;
+    }
+    if(placeBool){
+        x = x+1;
+    }
+    if(yearsBool){
+        x = x+1;
+    }
+    if(years2Bool){
+        x = x+1;
+    }
+    document.getElementById('progress_bar').style.width = `${x/6*100}%`
+}
+const data = [
+    {item:'2019'},
+    {item:'2020'},
+    {item:'2021'},
+    {item:'2022'},
+]
+const li = data.map((item,index)=>(
+    document.createElement('li').innerHTML=item.item
+))
+document.getElementById('select_options').appendChild(li)
